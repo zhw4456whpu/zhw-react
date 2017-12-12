f2eeb6b64ce4214f5f88e5437856b7c39fdccb5e
zhw4456whpu <zhw4456whpu@163.com>
2017-12-13 02:22:43 +0800

    新建react框架，centos环境

---
+++ app/main.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var AppComponent = require('./components/productBox.jsx');
+ReactDom.render(
+    <AppComponent / > 
+    , document.getElementById('content')
+);
