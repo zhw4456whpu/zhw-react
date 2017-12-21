c37e6aae0559557119212da06e2bf42ce8d27e48
zhw4456whpu <zhw4456whpu@163.com>
2017-12-22 01:32:25 +0800

    时间：2017/12/20 内容：新增input输入框获取焦点的功能，按钮click事件 提交人：winterlz

---
+++ app/components/Input.jsx
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+class Input extends Component{
+	clickHandler(){
+		alert(Math.random()*10);
+		this.refs.inputText.focus();
+		
+	}
+	
+
+	/**
+	 * onClick事件必须bind this，不然报错 TypeError: this is undefined
+	 * 2017-12-22
+	 **/
+	render() {
+		//var clickHandler = ()=>this.refs.inputText.focus();
+		return(
+			<div className="id">
+				<input type="text" ref="inputText" />
+				<input type="button" onClick={this.clickHandler.bind(this)} value="OK" />
+			</div>
+		)
+	}
+}
+
+export default Input;
